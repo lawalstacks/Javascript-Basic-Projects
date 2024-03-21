ceb2ea14acc9702373bc97edbd51a2846ea5718a
Kazeem Lawal <quazeemastic@gmail.com>
2024-03-21 17:14:36 +0100

    new changes

---
+++ javascript-dice-roller-with-U.I/index.js
@@ -0,0 +1,19 @@
+cc
+//Dice Roller
+
+function rolldice(){
+  const diceno = document.getElementById("diceno");
+const diceresult = document.getElementById("diceresult");
+const diceimg = document.getElementById("diceimg");
+const Values = [] ;
+const Diceimg =[];
+
+  for(let i = 0; i < diceno.value; ++i)
+  {
+    const value = Math.floor(Math.random() * 6 + 1);
+    Values.push(value);
+    Diceimg.push(`<img src="assets/${value}.png" alt="Dice${value}">`);
+  }
+diceresult.textContent =`Dice: ${Values.join(', ')}`;
+diceimg.innerHTML=`${Diceimg.join('')}`;
+}
